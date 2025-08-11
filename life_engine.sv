14e322bfd3d52252c8f53660664742f05c4eeec7
Eric Pearson <ericp@untether.ai>
2025-08-11 16:21:12 -0400

    initial draft added 2D life engine 32x32 multi generation engine with overlap neighbour rams.

--- life_engine.sv
+++ life_engine.sv
@@ -1,3 +1,262 @@
+// 2D Life engine
+// WIDTH x HEIGHT x GENS for each memory read. 
+// Memory depth = DEPTH
+module life_engine_2D #(
+	WIDTH = 32,		// Array width = Datapath width
+	DEPTH = 256,	// memory Depth
+	HEIGHT = 32,	// Datapath height
+	DBITS = 8,		// depth address bitwidth
+	GENS  = 2		// hardware Generations per pass
+) (
+	// System
+	input clk,
+	input reset,
+	// Memory Control
+	input logic [DBITS-1:0] raddr, // also used for init writes
+	input logic [DBITS-1:0] waddr,
+	input	logic we,
+	// cell array shift input
+	input logic sh,
+	// External Data Control
+	input logic ld,
+	input logic [4:0] ld_sel, // addressed row in the block
+	output logic [WIDTH-1:0] dout, // data out
+	// Init port
+	input logic init,
+	input logic [WIDTH*HEIGHT-1:0] init_data
+	
+);
+
+	// Memory
+	// 12 memories, 4 for array, 4 sides, 4 corners
+	// Fully registered 2 port rams
+	logic [11:0][255:0] mem_din, mem_dout;
+	genvar genii;
+	generate 
+		for( genii = 0; genii < 12; genii++ ) begin : _ram
+			cell_ram _cell_ram (
+				.clock( clk ),
+				.data( mem_din[genii] ),
+				.rdaddress( ( genii == 4 ) ? raddr[0][0] : // UL
+								( genii == 5 ) ? raddr[0][1] : // Top
+								( genii == 6 ) ? raddr[0][2] : // UR
+								( genii == 7 ) ? raddr[1][0] : // Left
+								( genii == 8 ) ? raddr[1][2] : // Right
+								( genii == 9 ) ? raddr[2][0] : // LL
+								( genii == 10) ? raddr[2][1] : // Bot
+								( genii == 11) ? raddr[2][2] : // LR
+								                 raddr[1][1] : // Center array
+				.wraddress( waddr ),
+				.wren( we[genii] ),
+				.q( mem_dout[genii] )
+			);
+		end
+	endgenerate
+
+	// Build the HEIGHT x WIDTH mem read array
+	logic [WIDTH-1:0][HEIGHT-1:0] mem_rd_array;
+	assign mem_rd_array = { mem_dout[3], mem_dout[2], mem_dout[1], mem_dout[0] };
+		
+	// Video Read port
+	logic [1:0] ld_del;
+	logic [1:0][4:0] ld_sel_del;
+	always_ff @(posedge clk) begin
+		ld_del[1:0] <= { ld_del[0], ld };
+		ld_sel_del[1:0] <= { ld_sel_del[0], ld_sel[2:0] };
+		if( ld_del[1] ) dout <= mem_rd_array[ld_sel_del[4:0]]; // latch row for video ***ASYNC after DELAY*** load and shift
+	end
+
+	// Build life data input array (with overlap) from the different memories
+	logic [HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0] cell_input;
+	always_comb begin
+		for( int yy = 0; yy < HEIGHT+2*GENS; yy++ ) begin
+			for( int xx = 0; xx < WIDTH+2*GENS; xx++ ) begin
+				if( xx >= GENS && xx < WIDTH+GENS && yy >= GENS && yy < 8+GENS )  //central 0
+					cell_input[yy][xx] = mem_dout[0][(yy-GENS)*32+(xx-GENS)];
+				else if	( xx >= GENS && xx < WIDTH+GENS && yy >= 8+GENS && yy < 16+GENS )  //central 1
+					cell_input[yy][xx] = mem_dout[1][(yy-GENS-8)*32+(xx-GENS)];
+				else if	( xx >= GENS && xx < WIDTH+GENS && yy >= 16+GENS && yy < 24+GENS )  //central 2
+					cell_input[yy][xx] = mem_dout[2][(yy-GENS-16)*32+(xx-GENS)];
+				else if	( xx >= GENS && xx < WIDTH+GENS && yy >= 24+GENS && yy < HEIGHT+GENS )  //central 3
+					cell_input[yy][xx] = mem_dout[3][(yy-GENS-24)*32+(xx-GENS)];
+				else if ( xx < GENS && yy < GENS )  // UL
+					cell_input[yy][xx] = mem_dout[4][(yy-0)*GENS+(xx-0)];
+				else if ( xx >= GENS && xx < WIDTH+GENS && yy < GENS )  // Top
+					cell_input[yy][xx] = mem_dout[5][(yy-0)*WIDTH+(xx-GENS)];
+				else if ( xx >= GENS+WIDTH && yy < GENS )  // UR
+					cell_input[yy][xx] = mem_dout[6][(yy-0)*GENS+(xx-GENS-WIDTH)];
+				else if ( xx < GENS && yy >= GENS && yy < GENS+HEIGHT ) // LEFT
+					cell_input[yy][xx] = mem_dout[7][(yy-GENS)*GENS+xx];
+				else if ( xx >= GENS+WIDTH && yy >= GENS && yy < GENS+HEIGHT // RIGHT
+					cell_input[yy][xx] = mem_dout[8][(yy-GENS)*GENS+(xx-GENS-WIDTH)];
+				else if ( xx < GENS && YY >= GENS+HEIGHT ) // LL
+					cell_input[yy][xx] = mem_dout[9][(yy-GENS-HEIGHT)*GENS+(xx-0)];
+				else if ( xx >= GENS && xx < WIDTH+GENS && yy >= GENS+HEIGHT ) // Bot
+					cell_input[yy][xx] = mem_dout[10][(yy-GENS-HEIGHT)*WIDTH+(xx-GENS)];
+				else /*if ( xx >= WIDTH+GENS && yy >= GENS+HEIGHT )*/ // LR
+					cell_input[yy][xx] = mem_dout[11][(yy-GENS-HEIGHT)*GENS+(xx-GENS-WIDTH)];
+			end // xx
+		end // yy	
+	end 
+	
+	// cell_arrays for generation life state
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0] cell_next; // comb Outputs of each generation	 
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0] cell_array;	 // reggister Inputs for each generation
+	always_ff @(posedge clk) begin
+		for( int yy = 0; yy < HEIGHT+2*GENS-2*gg; yy++ ) begin 
+			for( int xx = 0; xx < WIDTH+2*GENS-2*gg; xx++ ) begin
+				cell_array[0][yy][xx] <= cell_input[yy][xx];
+				for( int gg = 1; gg < GENS; gg++ ) begin
+					cell_array[gg] <= cell_next[gg-1];
+				end // gg
+			end //xx
+		end //yy
+	end // always
+	
+	// Input adders
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0][1:0] add3;	 // | shape	
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0][2:0] add4;	 // = shape
+	always_comb begin
+		for( int gg = 0; gg < GENS; gg++ ) begin
+			for( int yy = 1; yy < HEIGHT+2*GENS-2*gg-1; yy++ ) begin // no sums needed for first or last rows
+				for( int xx = 0; xx < WIDTH+2*GENS-2*gg-1; xx++ ) // =shape, skip last column
+					add4[gg][yy][xx] =  	{ 2'b00, cell_array[gg][yy-1][xx] } +
+											   { 2'b00, cell_array[gg][yy-1][xx+1] } +
+											   { 2'b00, cell_array[gg][yy+1][xx] } +
+											   { 2'b00, cell_array[gg][yy+1][xx+1] };
+											
+				for( int xx = 0; xx < WIDTH; xx++ ) // |shape, every column
+					add3[gg][yy][xx]  = 	{ 1'b0, cell_array[gg][yy-1][xx] } +
+												{ 1'b0, cell_array[gg][yy  ][xx] } +
+												{ 1'b0, cell_array[gg][yy+1][xx] } ;
+			end // yy
+		end //gg
+	end
+	
+
+	// arrange the inputs for the add8's as linear array
+	logic [GENS*(WIDTH+2*GENS)*(HEIGHT+2*GENS-1):0][5:0] add8_in;	// line up all the add8's inputs
+	always_comb begin
+		int add8_in = 0;
+		int add8_idx = 0;
+		for( int gg = 0; gg < GENS; gg++ ) begin // generations
+			for( int yy = gg; yy < HEIGHT+2*GENS-gg-1; yy++ ) begin // narrowing width
+				for( int xx = gg; xx < WIDTH+2*GENS-gg-1; xx++ ) begin // narrowing width
+					add8_in[add8_idx++] = 
+						(xx%3==0) ? { add4[gg][yy][xx  ], add3[gg][yy][xx-1], cell_array[gg][yy][xx+1] } : // |=*
+						(xx%3==1) ? { add4[gg][yy][xx  ], add3[gg][yy][xx-1], cell_array[gg][yy][xx+1] } : // |=*
+					 /*(xx%3==2)*/ { add4[gg][yy][xx-1], add3[gg][yy][xx+1], cell_array[gg][yy][xx-1] } ; // *=|
+				end // xx
+			end // yy
+		end // gg
+	end
+	
+	
+	// generate Add8's with hardcoded add431_cell with qunituple (5x) adders
+	
+	logic [GENS*(WIDTH+2*GENS)*(HEIGHT+2*GENS)-1+5:0][2:0] add8_out_a;
+	genvar kk;
+	generate
+		for( kk = 0; kk < (GENS*(WIDTH+2*GENS)*(HEIGHT+2*GENS)); kk+=5 ) begin : _gen431
+			add431_cell _add431(
+				.in	( {   ((kk+4)>=GENS*WIDTH)?6'h00:add8_in[kk+4], 
+								((kk+3)>=GENS*WIDTH)?6'h00:add8_in[kk+3], 
+								((kk+2)>=GENS*WIDTH)?6'h00:add8_in[kk+2], 
+								((kk+1)>=GENS*WIDTH)?6'h00:add8_in[kk+1], 
+								((kk+0)>=GENS*WIDTH)?6'h00:add8_in[kk+0] } ),
+				.add8  ( add8_out_a[kk+4-:5] )
+			);
+		end
+	endgenerate
+				
+	// unpack the add8 outputs
+	logic [GENS*(WIDTH+2*GENS)*(HEIGHT+2*GENS)-1+5:0][2:0] add8_out_b;
+	always_comb begin
+		int add8_idx = 0;
+		for( int gg = 0; gg < GENS; gg++ ) begin // generations
+			for( int yy = gg; yy < HEIGHT+2*GENS-gg-1; yy++ ) begin // narrowing width
+				for( int xx = gg; xx < WIDTH+2*GENS-gg-1; xx++ ) begin // narrowing width
+					add8_out_b[gg][yy][xx] = add8_out_a[add8_idx++];
+				end // xx
+			end // yy
+		end // gg
+	end
+		
+	// Register and connect up add8 outputs and delay orig accordingly
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0][2:0] add8; // final add8
+	logic [0:GENS-1][HEIGHT+2*GENS-1:0][WIDTH+2*GENS-1:0]      orig; // de	
+	always_ff @(posedge clk) begin
+		for( int gg = 0; gg < GENS; gg++ ) begin // generations
+			for( int yy = gg; yy < HEIGHT+2*GENS-gg-1; yy++ ) begin // narrowing width
+				for( int xx = gg; xx < WIDTH+2*GENS-gg-1; xx++ ) begin // narrowing width
+					add8[gg][yy][xx] <= add8_out_a[gg][yy][xx];
+					orig[gg][yy][xx] <= cell_array[gg][yy][xx]; // and delay orig cell to keep aligned					
+				end // xx
+			end // yy
+		end // gg
+	end
+
+	// Cell life state calculated 
+	always_comb begin : _life_cell
+		for( int gg = 0; gg < GENS; gg++ ) begin
+			for( int xx = gg; xx < WIDTH+2*GENS-gg-1 ; xx++ ) begin
+				for( int yy = gg; yy < HEIGHT+2*GENS-gg-1; yy++ ) begin // ***LIFE***
+					cell_next[gg][yy][xx] = ((( add8[gg][yy][xx]==3 ) &&  orig[gg][yy][xx] ) ||  // rule: alive and 3 neighbours --> stay alive
+													 (( add8[gg][yy][xx]==2 ) &&  orig[gg][yy][xx] ) ||  // rule: alive and 2 neighbours --> stay Alive
+													 (( add8[gg][yy][xx]==3 ) && !orig[gg][yy][xx] ))    // rule:  dead and 3 neighbours --> newly Alive
+																										? 1'b1 : 1'b0; // otherwise the cell dies or remains dead.
+				end // yy
+			end // xx
+		end // gg
+	end
+
+	
+	// Final Generation output reg is ram write data
+	logic [HEIGHT-1:0][WIDTH-1:0] out; // final registered output generation	 
+	always_ff @(posedge clk) begin 
+		for( int xx = 0; xx < WIDTH; xx++ ) 
+			for( int yy = 0; yy < HEIGHT; yy++ ) 
+				out[yy][xx] <= cell_next[GENS-1][yy+GENS][xx+GENS];
+	end
+			
+	// Format write data for memory inputs with boundary replication and cross wiring
+	always_comb begin
+		// mem_din 0,1,2,3 are the main array
+		for( int xx = 0; xx < WIDTH; xx++ ) begin
+			for( int yy = 0; yy < 8; yy++ ) begin
+				mem_din[0][yy*WIDTH+xx] = out[   yy][xx];
+				mem_din[1][yy*WIDTH+xx] = out[8 +yy][xx];
+				mem_din[2][yy*WIDTH+xx] = out[16+yy][xx];
+				mem_din[3][yy*WIDTH+xx] = out[24+yy][xx];
+			end // yy
+		end // gg
+		// Sides
+		for( int gg = 0; gg < GENS; gg ++ ) begin
+			for( int ii = 0; ii < 32; ii++ ) begin
+				mem_din[ 5][gg*WIDTH+ii] = out[HEIGHT-GENS+gg][ii]; // Top is taken from botton
+				mem_din[10][gg*WIDTH+ii] = out[gg][ii];             // Bot is taken from top
+				mem_din[ 7][gg+ii*GENS]  = out[ii][WIDTH-GENS+gg];	// Left is taken from right
+				mem_din[ 8][gg+ii*GENS]  = out[ii][gg];				// Right is taken from left
+			end // ii
+		end // gg
+		// Corners
+		for( int xx = 0; xx < GENS; xx++ ) begin
+			for( int yy = 0; yy < GENS; yy++ ) begin
+				mem_din[ 4][yy*GENS+xx] = out[HEIGHT-GENS+yy][WIDTH-GENS+xx]; 	// UL gets LR
+				mem_din[ 6][yy*GENS+xx] = out[HEIGHT-GENS+yy][           xx]; 	// UR gets LL
+				mem_din[ 9][yy*GENS+xx] = out[            yy][WIDTH-GENS+xx]; 	// LL gets UR
+				mem_din[11][yy*GENS+xx] = out[            yy][           xx]; 	// LR gets UL
+			end // xx
+		end // yy
+	end
+
+endmodule // life_engine_2D	
+
+/////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+
+
 // 1.5D Life engine
 // WIDTH x HEIGHT array of life cells spanning the full with of the arrya
 // GENS copies of the block to give multiple generations.
