baff3be2adad1d645f7d54ca27e4d71ba83e28e9
Eric Pearson <ericp@untether.ai>
2025-12-09 15:55:06 -0500

    basic hardware verilog for Day 8 part 2 data path written. need to go through and get control signals in place and time

--- life_core.sv
+++ life_core.sv
@@ -311,31 +311,160 @@ assign speaker_n = !speaker;
 	
 	// Day 8 part 2 hardware
 	// Process
-	// init color table w1r2 mem (pair)
-	// Map the 2nd color table read to raster access during right window window
-	// wait button press
-	// find next shortest path (full search boxs pairs)
+	// init color table 2R1W mem 
+	// (C) Dedicate one color table to raster access during right window window
+	// (S) wait button press
+	// (C) find next shortest path (full search boxs pairs)
 	// Take a r/w pass thru the color table
-	// if path sits on two different colors map all higher color to the lower color
-	// and check if all colors are now == 0 and set done
-	// if not done and long_press loop back for next shorted path pass.
-	// else if not done wait button press and loop back for next shortest pass
-	// done
+	// if box pair sits on the two different colors (2 color table reads) map all higher color to the lower color
+	// and after mapping check if all colors are now == 0 and set done
+	// (S) if not done and long_press loop back for next shorted path pass.
+	// (S) else if not done wait button press and loop back for next shortest pass
+	// (S) if button step back to init color table
 	
+	// Connect to fire button
+	logic vid_short, vid_long, vid_press;
+	debounce _vid_but ( .clk( hdmi_clk ), .reset( reset ), .in( fire_button ), .out( vid_short ), .long( vid_long ));
+	always @(posedge hdmi_clk)
+		vid_but <= vid_short;
+	assign vid_press = vid_short & ~~vid_but;
+
+	// Sequencer 
+	localparam S_IDLE 	= 0; // goto init
+	localparam S_INIT 	= 1; // Write a ramp 0 to 1023 into color table
+	localparam S_BUTTON 	= 2; // wait for a button press
+	localparam S_SEARCH 	= 3; // do the n*n/2 full search for next shortest pair
+	localparam S_LOOKUP1	= 4; // lookup 2colors of the pair
+	localparam S_LOOKUP2	= 5; // lookup 2colors of the pair
+	localparam S_VECMAP 	= 6; // walk thru color table and map the pair and test is its all color==0
+	localparam S_DONE 	= 7; // if not done a long or short button press got0 next full search, else done and button goto idle
+	logic [3:0] state;
 	
+	logic init_done;	// 1024 cyels to write a ramp into color table
+	logic search_done; // search next, 100K cycles
+	logic map_done; // remap color tables and check if alldone 1024 cycles
+	logic done; // completed part 2
+	always_ff @(posedge hdmi_clk) begin
+		if( reset ) begin
+			state <= S_IDLE;
+		end else begin
+			case( state ) 
+			S_IDLE 	: begin state <=                                          S_INIT           	  ; end
+			S_INIT 	: begin state <= (  init_done  								) ? S_BUTTON  : S_INIT 	  ; end
+			S_BUTTON : begin state <= (  vid_long || vide_press  				) ? S_SEARCH  : S_BUTTON  ; end
+			S_SEARCH : begin state <= (  search_done                       ) ? S_LOOKUP1 : S_SEARCH  ; end
+			S_LOOKUP1: begin state <=                                          S_LOOKUP2             ; end
+			S_LOOKUP2: begin state <=                                          S_VECMAP              ; end
+			S_VECMAP : begin state <= (  map_done                          ) ? S_DONE    : S_VECMAP  ; end
+			S_DONE 	: begin state <= ( !done && ( vid_long || vid_press   ) ? S_SEARCH  : 
+			                          (  done               && vid_press   ) ? S_INIT    : S_DONE    ; end
+			default  : begin state <= 4'bxxxx;
+			endcase
+		end
+	end
+
+	// Video copy of Color table (always read to screen at raster rate) displayed live (during 40? sec solve) on right window
+	// Index by raster position (an 8x8 cell location per box) and then generate the box RGB based on the indexed color.
+	logic [9:0]  ctable [0:1023];
+	logic [9:0] vctable [0:1023];
+	logic [9:0] vcolor;
+	logic [9:0[ vidx;
+	logic [7:0] vxcnt, vycnt;
+	assign vxcnt = xcnt-384;
+	assign vycnt = ycnt-128;
+	assign vidx[9:0] = { vycnt[7:-5],vxcnt[7-:5] }; // index color tabel by screen location
+	always_ff @(posedge hdmi_clk)
+		vcolor <= vctable[ vidx ];
+	logic [7:3] xcolor, ycolor;
+	assign { ycolor, xcolor } = vcolor;
+		
+	logic [7:0] colr, colg, colb; // rgb
+	assign { colr, colg, colb } = { { ycolor[3], xcolor[4], ycolor[6], ycolor[3], xcolor[4], ycolor[6], ycolor[3], xcolor[4] },
+	                               ~{ ycolor[4], xcolor[5], ycolor[6], xcolor[7], ycolor[4], xcolor[5], ycolor[6], xcolor[7] },
+										     { xcolor[3], ycolor[5], xcolor[7], xcolor[3], ycolor[5], xcolor[7], xcolor[3], ycolor[5] } };
 	
-	
-	
-	
-	// Create display window and RGB
+	// Create left reference display window and RGB
 	logic window;
 	logic [7:0] winr, wing, winb;
 	assign window = ( active_left || active_right ) ? 1'b1 : 1'b0; // two display windows
 	assign { winr, wing, winb } = { { ycnt[3], xcnt[4], ycnt[6], ycnt[3], xcnt[4], ycnt[6], ycnt[3], xcnt[4] },
 	                               ~{ ycnt[4], xcnt[5], ycnt[6], xcnt[7], ycnt[4], xcnt[5], ycnt[6], xcnt[7] },
-											  { xcnt[3], ycnt[5], xcnt[7], xcnt[3], ycnt[5], xcnt[7], xcnt[3], ycnt[5] } };
-	
-	//////////////////////////////// display done //////////////////////////////////
+											  { xcnt[3], ycnt[5], xcnt[7], xcnt[3], ycnt[5], xcnt[7], xcnt[3], ycnt[5] } };						
+											
+	// full pair search
+		logic [63:0] thresh; // min cost threshold
+		logic [63:0] distance; // pair cost^2
+		logic [63:0] cost; // best cost so far
+		logic [63:0] last_candidate; // reported puzzle sum, when we finish!
+		
+			logic [9:0] a_count, bcount; 
+			logic search_run;
+			assign search_run = ( state == S_SEARCH ) ? 1'b1 : 1'b0;
+			assign search_done = ( a_count == 998 && b_count == 999 ) ? 1'b1 : 1'b0;
+			always_ff @(posedge hdmi_clk) begin
+			if( reset ) begin
+				a_count <= 0; // lead couner
+				b_count <= 1; // upper counter
+				search_done <= 0;
+			end else begin
+				a_count <= ( search_run && search_done    ) ? 0 : 
+				           ( search_run && b_count == 999 ) ? a_count + 1 :
+							                                     a_count;
+				b_count <= ( search_run && search_done    ) ? 1 :
+				           ( search_run && b_count == 998 ) ? a_count + 2 :
+							                                     b_count:
+				// Coord memory reads
+				box1 <= box_ram1[a_count];
+				box2 <= box_ram2[b_count];
+				
+				// Calc x,y,z differenced
+				dx <= ( box1[53-:18] > box2[53:18] ) ? ( box1[53-:18] - box2[53:18] ) : ( box2[53-:18] - box1[53:18] );
+				dy <= ( box1[35-:18] > box2[35:18] ) ? ( box1[35-:18] - box2[35:18] ) : ( box2[35-:18] - box1[35:18] );
+				dz <= ( box1[17-:18] > box2[17:18] ) ? ( box1[17-:18] - box2[17:18] ) : ( box2[17-:18] - box1[17:18] );
+				
+				// Calculate squared valued
+				dx2 <= dx * dx;
+				dy2 <= dy * dy;
+				dz2 <= dz * dz;
+				
+				// Calculate pair distance
+				distance <= dx2 + dy2 + dz2;
+				
+				// Update best
+				cost <= ( !search_run ) ? (10000*10000)*3 : 
+				        ( search_run && distance > thresh && distance < cost ) ? distance : cost;
+				best_a <= ( search_run && distance > thresh && distance < cost ) ? a_count_del[4] : best_a;
+				best_b <= ( search_run && distance > thresh && distance < cost ) ? b_count_del[4] : best_b;
+				
+				// update thresh when done
+				thresh <= ( state == S_INIT ) ? 0 : ( search_run_d[5] && !search_run_d[4] ) ? cost;  
+			end
+		end
+				
+				
+		// Color table operations, write to both banks, but only read 1
+		// lookup 2 (two) colors for best_a and best_b and get min/max during S_LOOKUP1/2
+		// walk and init color table during S_INIT
+		// walk, map, test color table during S_VECMAP
+	// Color table burst writes (init, map&test) A
+	logic [9:0] ctable_addr;
+
+	always_ff @(posedge hdmi_clk) begin
+		c_count <= ( state == S_IDLE  || state == S_LOOKUP2 ) ? 0 :
+					  ( state == S_INIT  || state == S_VECMAP  ) ? c_count + 1 : c_count
+		ctable_addr <= ( state == S_LOOKUP1 ) ? best_a :
+		               ( state == S_LOOKUP2 ) ? best_b : c_count ;
+		read_color <= ctable[ ctable_addr ];A
+		if( state == S_INIT || state == S_VECMAP ) begin
+			ctable[ctable_waddr] <= ( S_INIT ) ? c_count : mapped;
+		  Vctable[ctable_waddr] <= ( S_INIT ) ? c_count : mapped;
+		end	
+	end
+			                                                                        //
+	//                                                                            //
+	////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////// AoC Day 8 done ////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
 	// Font Generator
 	logic [7:0] char_x, char_y;
