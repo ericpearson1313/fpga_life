22a707c957881fe24efcf30bd24425df0007a2a7
Eric Pearson <ericp@untether.ai>
2025-12-12 14:34:02 -0500

    Change to 22 bits sufficient for part 2

--- aoc_day11.sv
+++ aoc_day11.sv
@@ -2,13 +2,13 @@
 module aoc_day11 (
 	input logic clk,
 	input logic reset,
-	input logic  [11:0] out,
-	input logic  [11:0] fft_ofs,
-	input logic  [11:0] dac_ofs,
-	output logic [11:0] svr,
-	output logic [11:0] you,
-	output logic [11:0] fft,
-	output logic [11:0] dac
+	input logic  [21:0] out,
+	input logic  [21:0] fft_ofs,
+	input logic  [21:0] dac_ofs,
+	output logic [21:0] svr,
+	output logic [21:0] you,
+	output logic [21:0] fft,
+	output logic [21:0] dac
 	);
 
 `include "day11_declaration.sv"
